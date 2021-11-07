823c1b74a2b74b1e5d93c4b43bb70d5a956bb1ee
josue.duarte <duarte.josue@hotmail.com>
2021-11-06 21:07:34 -0600

    update

--- main.sh
+++ main.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 echo "Hello"
-apt-get update -y
-apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-#docker build -t dockernginx .
+docker build -t dockernginx .
 
